97589a8dc1c903ff8b371ce03375d69b097c5009
Jim Shaddix <jimmy.shaddix2.0@gmail.com>
2019-07-13 22:10:58 -0600

    init

---
+++ DTabs.py
@@ -0,0 +1,104 @@
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+
+tabs = \
+    html.Div(id='manhattan-control-tabs', className='control-tabs', children=[
+            dcc.Tabs(id='manhattan-tabs', value='what-is', children=[
+                dcc.Tab(
+                    label='About',
+                    value='what-is',
+                    children=html.Div(className='control-tab', children=[
+                        html.H4(className='what-is', children="What's Neutron Scattering All About?"),
+                        html.P(
+                               'Neutron scattering, the irregular dispersal of free ' 
+                               'neutrons by matter, can refer to either the naturally '
+                               'occurring physical process itself or to the man-made '
+                               'experimental techniques that use the natural process '
+                               'for investigating materials. The natural/physical phenomenon'
+                               'is of elemental importance in nuclear engineering and '
+                               'the nuclear sciences. Regarding the experimental technique,'
+                               'understanding and manipulating neutron scattering is fundamental '
+                               'to the applications used in crystallography, physics, physical chemistry,'
+                               ' biophysics, and materials research.'),
+                        html.P('Neutron scattering is practiced at research reactors and '
+                               'spallation neutron sources that provide neutron radiation'
+                               'of varying intensities. Neutron diffraction (elastic scattering)'
+                               'techniques are used for analyzing structures; where inelastic neutron scattering '
+                               'is used in studying atomic vibrations and other excitations.')
+                    ])
+                ),
+                dcc.Tab(
+                    label='Graph',
+                    value='graph',
+                    children=html.Div(className='control-tab', children=[
+                        html.Div(className='app-controls-block', children=[
+
+
+                            # scroll-bar and input field
+                            dbc.Row([
+
+                                # title and scrollbar
+                                dbc.Col([
+
+                                    html.Div(
+                                        html.P("Scan Axis:")
+                                    ),
+
+                                    # axis-dropdown
+                                    dcc.Dropdown(
+                                        options=[
+                                            {'label': 'X', 'value': 'x'},
+                                            {'label': 'Y', 'value': 'y'}
+                                        ],
+                                        value='y',
+                                        id="dropdown-axis"
+                                    ),
+
+                                ], width=6),
+
+
+                                # title and input field
+                                dbc.Col([
+
+                                    html.Div(
+                                        html.P("Axis Value:")
+                                    ),
+
+                                    dcc.Input(
+                                        placeholder='0',
+                                        type='number',
+                                        readOnly="readOnly",
+                                        value='0',
+                                        id="input-axis-value"
+                                    )
+
+                                ], width=6)
+
+                            ]),
+
+                            html.Div(
+                                className='app-controls-name',
+                                children=[
+                                    'Threshold value (red)'
+                                ]
+                            ),
+                            dcc.Slider(
+                                id='mhp-slider-genome',
+                                className='control-slider',
+                                vertical=False,
+                                updatemode='mouseup',
+                                max=4,
+                                min=1,
+                                value=2,
+                                marks={
+                                    i + 1: '{}'.format(i + 1)
+                                    for i in range(4)
+                                },
+                                step=0.05
+                            ),
+                        ])
+                    ])
+                )
+            ])
+        ])
---
+++ main.py
@@ -0,0 +1,155 @@
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State
+import plotly.graph_objs as go
+import pandas as pd
+import numpy as np
+from DTabs import tabs
+
+# read in data
+df = pd.read_csv("Data/data_from_NaCaCo2F7_for_Plotly.csv", names=["x", "y", "z"])
+x_unique = df.x.unique()
+y_unique = df.y.unique()
+
+# Data for the Heat Map
+trace_heatmap = go.Heatmap(
+                    z=np.array(df.z).reshape(-1, (len(df.x.unique()))),
+                    colorscale='Viridis'
+                )
+
+# create application
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+# Layout
+app.layout = html.Div([
+
+    # title
+    html.Div([
+
+        html.A(
+            html.Img(src="assets/images/rosslab_logo.png", id="logo"),
+            href="http://www.rosslabcsu.com/"
+        ),
+
+        html.H1("Neutron Scattering", id="title"),
+        html.A(
+            html.P("View on Github", id="github-text"),
+            href="https://github.com/Jim-Shaddix",
+            id="github-link"
+        ),
+        html.Div(className="clr")
+    ], id="banner"),
+
+    tabs,
+
+    # Heat Map Row
+    dbc.Row([
+
+        # left col
+        dbc.Col([
+
+            # heat-map
+            dcc.Graph(
+                id="graph-heatmap"
+            ),
+
+            # slider
+            html.Div([
+
+                dcc.Slider(
+                    id="slider-heatmap",
+                    value=1,
+                    updatemode="drag"
+                )
+
+            ], id="slider-stuff")
+
+        ], width=6),
+
+        # right col
+        dbc.Col([
+            dcc.Graph(id="graph-cross-section")
+        ], width=6)
+    ])
+
+])
+
+
+# update axis drop down
+@app.callback([Output('slider-heatmap', 'min'),
+              Output('slider-heatmap', 'max'),
+              Output('slider-heatmap', 'value'),
+              Output('slider-heatmap', 'marks')],
+              [Input('dropdown-axis', 'value')])
+def update_axis_dropdown(value):
+
+    if value == "x":
+        max_val = len(x_unique)-1
+    else:
+        max_val = len(y_unique)-1
+
+    marks = [10]
+    while (marks[-1] + 10) < max_val:
+        marks.append(marks[-1] + 10)
+
+    marks = dict(zip(marks, [str(i) for i in marks]))
+
+    return 1, max_val, 1, marks
+
+
+# update slider
+@app.callback([Output('graph-heatmap', 'figure'),
+               Output('graph-cross-section', 'figure'),
+               Output('input-axis-value', 'value')],
+              [Input('slider-heatmap', 'value')],
+              [State('dropdown-axis', 'value')])
+def update_slider(slider_value, dropdown_value):
+
+    # red line plot values
+    if dropdown_value == "x":
+        x = [slider_value] * 2
+        y = [0, len(y_unique)-1]
+    else:
+        x = [0, len(x_unique)-1]
+        y = [slider_value] * 2
+
+    # Find cross section plot parameters
+    if dropdown_value == "x":
+        new_df = df[df.x == x_unique[slider_value]]
+        y_cross = new_df.z
+        x_cross = new_df.y
+        x_label = "Y-Values"
+    else:
+        new_df = df[df.y == y_unique[slider_value]]
+        y_cross = new_df.z
+        x_cross = new_df.x
+        x_label = "X-Values"
+
+    # heatmap figure
+    figure=go.Figure(
+        data=[trace_heatmap, go.Scatter(x=x, y=y, marker={"color": "red"})],
+        layout=go.Layout(
+            title='Intensity',
+            #autosize=True,
+            #width=700,
+            #height=700,
+            paper_bgcolor='rgba(0,0,0,0)',
+            plot_bgcolor='rgba(0,0,0,0)'
+        )
+    )
+
+    return figure, {'data': [go.Scatter(x=x_cross, y=y_cross)],
+                    'layout':{
+                        'title':'Cross Section',
+                        #autosize=True,
+                        #width=700,
+                        #height=700,
+                        'paper_bgcolor':'rgba(0,0,0,0)',
+                        'plot_bgcolor':'rgba(0,0,0,0)'
+                    }}, slider_value
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
