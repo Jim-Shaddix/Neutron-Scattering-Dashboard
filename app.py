535a4ab7bae4c95ec6265d140e6692828965d51e
Jim Shaddix <jimmy.shaddix2.0@gmail.com>
2019-11-28 18:07:01 -0700

    renamed DTabs.py to tabs.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-from DTabs import tabs
+from tabs import tabs
 """
 This file initializes the application and formats
 how the application gets displayed.
